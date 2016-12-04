fbd5fac395a912f4d5fc4a74a7d4331bf8003f68
Maik Ellerbrock <opensource@frapsoft.com>
2016-12-04 23:59:16 +0100

    refactor(refactor): comment few things, changed test file to test.yml, log also ok links for testing

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict';
 
-// - tests if the repository is available / online
-// - optional: check the current version vs. the github version
-//   to activate the check set versionCheck in line 14 to true
+//  tests if the repository is available / online
 //
+//  optional: check the current version vs. the github version
+//  to activate the check set versionCheck in line 14 to true
+
 
 const yaml = require('js-yaml');
 const fs = require('fs');
 const request = require('request');
 const log = require('console-emoji');
 
-const fileName = 'plugins.yml';
+const fileName = 'test.yml'; // <= change to plugins.yml before going life
 const selector = 'repo';
-const versionCheck = true;
+const versionCheck = true;  // <= optional: check for version changes
 const vselector = 'tag';
 const gitRaw = 'https://raw.githubusercontent.com';
 const branch = 'master';
@@ -30,50 +31,51 @@ for (let x = 0, len = modules.length; x < len; x++) {
 
   if (link !== 'undefined') {
     request(link, {method: 'HEAD'}, (err, res) => {
-      if (err) {
-        throw new Error(err);
-      }
-      if (res.statusCode === 200) {
-        // log(modules[x], 'ok');
+        if (err) {
+          throw new Error(err);
+        }
+        if (res.statusCode === 200) {
+          log(modules[x], 'ok');
 
           // check for version changes (optional)
-        let search = 'github.com/';
-        let tmp = link.substring(link.indexOf(search) + search.length);
+          let search = 'github.com/';
+          let tmp = link.substring(link.indexOf(search) + search.length);
 
-        // check for github only
-        let isGitUrl = link.includes(search);
+          // currently only check if its a github link
+          let isGitUrl = link.includes(search);
 
-        if (versionCheck && isGitUrl) {
-          if (tmp.lastIndexOf('.git')) {
-            let part = tmp.substring(0, tmp.length - 4);
-            let cut = part.indexOf('/');
+          if (versionCheck && isGitUrl) {
+            if (tmp.lastIndexOf('.git')) {
+              let part = tmp.substring(0, tmp.length - 4);
+              let cut = part.indexOf('/');
 
               // Username / Repository maybe useful for later tests ...
-            let user = part.substr(0, cut);
-            let repo = part.substr(cut + 1);
-            let pkgUrl = `${gitRaw}/${user}/${repo}/${branch}/package.json`;
+              let user = part.substr(0, cut);
+              let repo = part.substr(cut + 1);
+              let pkgUrl = `${gitRaw}/${user}/${repo}/${branch}/package.json`;
 
-            request(pkgUrl, {method: 'GET'}, (err, res, body) => {
-              if (err) {
-                throw new Error(err);
-              }
-              if (res.statusCode === 200) {
-                const pkg = JSON.parse(body);
-                if (semver !== pkg.version) {
-                  log(`Version changed for ${  modules[x]  } ${  semver  } => ${  pkg.version}`, 'warn');
+              request(pkgUrl, {method: 'GET'}, (err, res, body) => {
+                if (err) {
+                  throw new Error(err);
                 }
-              }
-            });
+                if (res.statusCode === 200) {
+                  const pkg = JSON.parse(body);
+                  if (semver !== pkg.version) {
+                    log(`Version changed for ${  modules[x]  } ${  semver  } => ${  pkg.version}`, 'warn');
+                  }
+                }
+              });
+            }
           }
-        }
 
-      } else {
-        log(modules[x], 'err');
-        error = 1;
+        } else {
+          log(modules[x], 'err');
+          error = 1;
+        }
       }
-    }
     );
   }
 }
 
+// return 0 when no dead links found
 return error;
